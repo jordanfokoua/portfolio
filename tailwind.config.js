49aa9663385655da99936b8e08bc5160a400c43f
blvck237 <jfokoua@gmail.com>
2024-01-28 21:05:44 +0100

    Setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./src/**/*.{js,jsx,ts,tsx}',
-	],
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		colors: {
+			primary: '#f0db4f',
+			secondary: '#323330',
+			white: '#fff',
+			grey: '#999',
+			'background-1': '#323330',
+			'background-2': '#262524',
+			'background-3': '#1F1F1E',
+			'background-4': '#000',
+		},
+		fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Merriweather', 'serif'],
+		},
+		extend: {
+			spacing: {
+				128: '32rem',
+				144: '36rem',
+			},
+			borderRadius: {
+				'4xl': '2rem',
+			},
+		},
 	},
 	plugins: [],
 };

5fcf818669cc22c49f0861a7477cbbd7297e4828
blvck237 <jfokoua@gmail.com>
2024-01-31 20:44:23 +0100

    📱 Make Social Icons responsive using Tailwind

--- src/components/SocialIcons/SocialIcons.tsx
+++ src/components/SocialIcons/SocialIcons.tsx
@@ -1,46 +1,49 @@
 import React from 'react';
-import styles from './SocialIcons.module.css';
 import { PROFILE } from '../../constants/profile';
+
+const socialIcons = [
+	{
+		name: 'GitHub',
+		href: 'https://github.com/' + PROFILE.GITHUB,
+		iconClass: 'fab fa-github',
+	},
+	{
+		name: 'Medium',
+		href: 'https://medium.com/' + PROFILE.MEDIUM,
+		iconClass: 'fab fa-medium',
+	},
+	{
+		name: 'LinkedIn',
+		href: 'https://www.linkedin.com/in/' + PROFILE.LINKEDIN,
+		iconClass: 'fab fa-linkedin',
+	},
+	{
+		name: 'Twitter',
+		href: 'https://twitter.com/' + PROFILE.TWITTER,
+		iconClass: 'fab fa-twitter',
+	},
+	{
+		name: 'Email',
+		href: 'mailto:' + PROFILE.EMAIL,
+		iconClass: 'fas fa-envelope',
+	},
+];
 
 const SocialIcons: React.FC = () => {
 	return (
-		<div className={styles.socialIcons}>
-			<a
-				href={'https://github.com/' + PROFILE.GITHUB}
-				target="_blank"
-				rel="noopener noreferrer"
-				className={styles.icon}
-			>
-				<i className="fab fa-github"></i>
-			</a>
-			<a
-				href={'https://medium.com/' + PROFILE.MEDIUM}
-				target="_blank"
-				rel="noopener noreferrer"
-				className={styles.icon}
-			>
-				<i className="fab fa-medium"></i>
-			</a>
-			<a
-				href={'https://www.linkedin.com/in/' + PROFILE.LINKEDIN}
-				target="_blank"
-				rel="noopener noreferrer"
-				className={styles.icon}
-			>
-				<i className="fab fa-linkedin"></i>
-			</a>
-			<a
-				href={'https://github.com/' + PROFILE.TWITTER}
-				target="_blank"
-				rel="noopener noreferrer"
-				className={styles.icon}
-			>
-				<i className="fab fa-twitter"></i>
-			</a>
-			<a href={'mailto:' + PROFILE.EMAIL} className={styles.icon}>
-				<i className="fas fa-envelope"></i>
-			</a>
-			<div className={styles.line}></div>
+		<div className="hidden md:flex flex-col items-center fixed left-0 bottom-0 p-5 z-50 space-y-4">
+			{socialIcons.map((icon) => (
+				<a
+					key={icon.name}
+					href={icon.href}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-white text-xl transition-colors duration-300 hover:text-primary hover:scale-110"
+				>
+					<i className={icon.iconClass}></i>
+				</a>
+			))}
+			<div className="w-px h-24 bg-white"></div>
 		</div>
 	);
 };

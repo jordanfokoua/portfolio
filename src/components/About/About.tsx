cfe8887c8a8ba347cf5ba241df372219fc73dc1f
blvck237 <jfokoua@gmail.com>
2024-02-01 10:04:07 +0100

    📱 Make About section responsive using Tailwind

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import styles from './About.module.css';
-import globalStyles from '../../styles/global.module.css';
 import Section from '../../shared/Section/Section';
 import { PROFILE } from '../../constants/profile';
+import globalStyles from '../../styles/global.module.css';
 
 const About: React.FC = () => {
 	return (
-		<Section id="about" className={`${styles.about} ${globalStyles.section}`}>
+		<Section id="about">
 			<h2 className={`${globalStyles.heading} ${globalStyles.sectionHeading}`}>
 				<span className={globalStyles.sectionNumber}>01.{'  '}</span>About Me
 			</h2>
-			<div className={globalStyles.sectionContent}>
-				<div className={styles.description}>
+			<div className={`flex flex-col md:flex-row justify-around`}>
+				<div className="w-full md:w-1/2 md:mb-0">
 					{PROFILE.ABOUT.map((paragraph, index) => (
-						<div key={index} className={styles.descriptionContent}>
+						<div key={index} className="mb-3">
 							{paragraph}
 						</div>
 					))}
 				</div>
-				<div className={styles.skillsBlock}>
-					Here are some technologies I've recently engaged with:
-					<ul className={styles.skills}>
+				<div className="flex flex-col">
+					<p>Here are some technologies I've recently engaged with:</p>
+					<ul className="columns-2 md:columns-2 list-none p-0 mt-2">
 						{PROFILE.SKILLS.map((skill, index) => (
-							<li key={index}>{skill}</li>
+							<li
+								key={index}
+								className="relative pl-5 mb-2 before:content-['+'] before:absolute before:left-0 before:top-0 before:text-primary"
+							>
+								{skill}
+							</li>
 						))}
 					</ul>
 				</div>

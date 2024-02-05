f309ec5654854edc65c2076612bb7e1681023ad6
blvck237 <jfokoua@gmail.com>
2024-02-05 18:24:45 +0100

    ✨ Add number and heading props to section component

--- src/shared/Section/Section.tsx
+++ src/shared/Section/Section.tsx
@@ -6,10 +6,12 @@ interface SectionProps {
 	children: ReactNode;
 	className?: string;
 	id: string;
+	number?: string;
+	heading?: string;
 }
 
 const Section: FC<SectionProps> = ({ children, ...props }) => {
-	const { id, className = '' } = props;
+	const { id, className = '', number, heading } = props;
 	const ref = useRef<HTMLDivElement>(null);
 	const isVisible = useOnScreen(ref);
 	const [hasAppeared, setHasAppeared] = useState(false);
@@ -24,10 +26,18 @@ const Section: FC<SectionProps> = ({ children, ...props }) => {
 		<section
 			id={id}
 			ref={ref}
-			className={`flex flex-col justify-center self-center items-center h-screen px-[10vw] border-t border-primary text-left text-white leading-normal ${className}`}
+			className={`flex flex-col justify-start md:justify-center h-screen px-[4vw] border-t border-primary text-left text-white leading-normal ${className}`}
 		>
 			{hasAppeared ? (
 				<FadeInOut show={true} duration={500} animation="in">
+					{heading && (
+						<h2 className="mb-8 text-3xl text-[#F0DB4F]">
+							<span className="text-white text-xl font-light mr-1">
+								{number ? number + '. ' : ''}
+							</span>
+							{heading}
+						</h2>
+					)}
 					{children}
 				</FadeInOut>
 			) : (

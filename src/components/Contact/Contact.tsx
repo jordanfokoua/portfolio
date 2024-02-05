045514d73a39906d43c51f80da242845969a12e5
blvck237 <jfokoua@gmail.com>
2024-02-05 22:16:17 +0100

    Make Contact responsive

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Button from '../../shared/Button/Button';
 import Section from '../../shared/Section/Section';
 import { PROFILE } from '../../constants/profile';
-import styles from './Contact.module.css';
 
 const Contact: React.FC = () => {
 	const sendEmail = () => {
@@ -10,10 +9,14 @@ const Contact: React.FC = () => {
 	};
 
 	return (
-		<Section id="contact" className='justify-center' heading="Let's Get In Touch" number="03">
-			<div className={`${styles.contact}`}>
-				<p>{PROFILE.CONTACT_MESSAGE}</p>
-				<Button onClick={sendEmail} label="Say Hello" className={`${styles.contactButton}`} />
+		<Section id="contact" className="justify-center" heading="Let's Get In Touch" number="03">
+			<div className="flex flex-col items-center">
+				<p className="text-center  mb-4">{PROFILE.CONTACT_MESSAGE}</p>
+				<Button
+					onClick={sendEmail}
+					label="Say Hello"
+					className="self-center bg-primary hover:bg-primary-dark text-white font-bold py-2 px-4 rounded"
+				/>
 			</div>
 		</Section>
 	);

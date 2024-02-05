b4281b7c6eb2a416668b8f145f017ba5f48962ce
blvck237 <jfokoua@gmail.com>
2024-02-05 18:26:59 +0100

    Make Experience timeline responsive

--- src/components/Experience/ExperienceTimeline.tsx
+++ src/components/Experience/ExperienceTimeline.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import styles from './ExperienceTimeline.module.css';
 import { EXPERIENCES } from '../../constants/experience';
 import { Experience } from '../../types/experience';
 import FadeInOut from '../../shared/FadeInOut';
@@ -19,51 +18,50 @@ const ExperienceTimeline = () => {
 	};
 
 	return (
-		<div className={styles.timelineContainer}>
-			<div className={styles.timelineDots}>
-				{}
+		<div className="flex flex-col md:flex-row h-full w-[91vw] mx-auto h-100vh justify-between items-start">
+			<div className="flex md:flex-col overflow-x-auto md:overflow-visible w-full md:w-1/6 text-left md:mb-0 md:px-0 whitespace-nowrap">
 				{EXPERIENCES.map((exp) => (
 					<div
 						key={exp.id}
-						className={styles.timelineItem}
+						className={`flex p-2 px-4 md:flex-col items-left mb-2 cursor-pointer text-gray-400 w-full text-sm  ${
+							selectedXp?.id === exp.id ? 'bg-gray-200 border-b-2 border-yellow-400 px-2' : ''
+						}`}
 						onClick={() => handleSelect(exp)}
 						role="button"
 						aria-label={`Select experience at ${exp.company}`}
 					>
-						<div
-							className={`${styles.timelineDot} ${
-								selectedXp?.id === exp.id ? styles.selected : ''
-							}`}
-						></div>
-						<span className={styles.companyName}>{exp.company}</span>
+						{exp.company}
 					</div>
 				))}
 			</div>
 
-			<div className={`${styles.xpContent}`}>
+			<div className="overflow-hidden transition-all w-full md:w-4/6 my-2 h-full">
 				<FadeInOut show={show} duration={200}>
-					<div className={`${styles.xpContentTitle}`}>
+					<div className="text-xl font-medium">
 						{selectedXp?.title}{' '}
 						<a
 							target="_blank"
 							rel="noopener noreferrer"
-							className={`companyHandle`}
+							className="companyHandle mb-1"
 							href={selectedXp?.company_url}
 							data-replace={'@ ' + selectedXp?.company}
 						>
 							<span> @ {selectedXp?.company}</span>
 						</a>
 					</div>
-					<span>{selectedXp?.year}</span>
-					<p>{selectedXp?.description}</p>
+					<span className="font-light text-sm">{selectedXp?.year}</span>
+					<p className="text-justify mt-2 hidden md:block"> {selectedXp?.description}</p>
 					{selectedXp?.details && (
-						<div className={styles.details}>
-							<ul>
-								{selectedXp?.details.map((detail: any) => (
-									<li key={detail}>{detail}</li>
-								))}
-							</ul>
-						</div>
+						<ul className="list-disc">
+							{selectedXp?.details.map((detail: any, index: number) => (
+								<li
+									key={index}
+									className="relative pl-5 mb-2 before:content-['+'] before:absolute before:left-0 before:top-0 before:text-primary"
+								>
+									{detail}
+								</li>
+							))}
+						</ul>
 					)}
 				</FadeInOut>
 			</div>

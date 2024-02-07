0f1461b0d5bfb09fd34f91f8b04424f66e2d8094
blvck237 <jfokoua@gmail.com>
2024-02-07 01:03:15 +0100

    Change menu display on mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -29,27 +29,37 @@ const menuItems: MenuItem[] = [
 const Navbar: React.FC = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
-	const handleMenuClick = (value: boolean) => {
-		setIsOpen(value);
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const closeMenu = () => {
+		setIsOpen(false);
 	};
 
 	return (
-		<nav
-			className="flex items-center justify-between py-4 px-4 text-white fixed top-0 left-0 w-full z-50 bg-background-4 backdrop-filter backdrop-blur-lg bg-opacity-40 border-b border-background-4"
-		>
+		<nav className="flex items-center justify-between py-4 px-4 text-white fixed top-0 left-0 w-full z-50 bg-background-4 backdrop-filter backdrop-blur-lg bg-opacity-40 border-b border-background-4">
 			<Logo />
 
-			<HamburgerMenu onClick={handleMenuClick} />
+			<HamburgerMenu onClick={toggleMenu} />
 
-			<ol
-				className={`absolute md:relative md:flex md:items-center top-full left-0 md:left-auto md:top-auto w-full md:w-auto bg-background-4 transition-all ease-out duration-300 transform ${
-					isOpen ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'
-				} md:translate-y-0 md:opacity-100`}
+			<div
+				className={`absolute top-0 left-0 h-screen w-full flex flex-col items-center justify-center bg-background-4 bg-opacity-95 z-40 transform transition-transform duration-300 ease-in-out ${
+					isOpen ? 'translate-x-0' : '-translate-x-full'
+				} md:hidden`}
 			>
 				{menuItems.map((item, index) => (
-					<li key={item.id} className="px-3 py-2 md:py-0">
-						<a href={item.link} className="block md:inline-block">
-							<span className="mr-2 font-bold">{`${index + 1}.`}</span>
+					<a key={item.id} href={item.link} className="text-xl text-white py-2" onClick={closeMenu}>
+						<span className="font-bold">{`${index + 1}.`}</span> {item.name}
+					</a>
+				))}
+			</div>
+
+			<ol className={`hidden md:flex md:items-center space-x-8`}>
+				{menuItems.map((item, index) => (
+					<li key={item.id}>
+						<a href={item.link} className="flex items-center" onClick={closeMenu}>
+							<span className="font-bold text-primary mr-2">{`${index + 1}.`}</span>
 							{item.name}
 						</a>
 					</li>
